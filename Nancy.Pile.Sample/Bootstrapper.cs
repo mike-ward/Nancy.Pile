9a4ce687ee3596d46b51e1c55dd4453b6c5ce375
Mike Ward <mike@mike-ward.net>
2014-06-29 14:58:31 -0400

    Add javascript example

--- Nancy.Pile.Sample/Bootstrapper.cs
+++ Nancy.Pile.Sample/Bootstrapper.cs
@@ -10,23 +10,20 @@ namespace Nancy.Pile.Sample
         {
             base.ConfigureConventions(nancyConventions);
 
-            nancyConventions.StaticContentsConventions.AddStylesBundle("styles.css", Bundle.IsDebugMode,
+            nancyConventions.StaticContentsConventions.AddStylesBundle("styles.css", !Bundle.IsDebugMode,
                 new[]
                 {
                     "css/pure.css",
                     "css/*.css"
                 });
 
-            //nancyConventions.StaticContentsConventions.AddScriptsBundle("scripts.js", Bundle.IsDebugMode,
-            //    new[]
-            //    {
-            //        "js/third-party/d3.min.js",
-            //        "js/third-party/lodash.min.js",
-            //        "js/third-party/angular.min.js",
-            //        "js/third-party/mousetrap.min.js",
-            //        "js/third-party/hotkeys.js",
-            //        "js/app/*.js"
-            //    });
+            nancyConventions.StaticContentsConventions.AddScriptsBundle("scripts.js", !Bundle.IsDebugMode,
+                new[]
+                {
+                    "js/third-party/*.js",
+                    "js/app.js",
+                    "js/app/*.js"
+                });
         }
     }
 }
