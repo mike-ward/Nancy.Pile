f64b03a2998e805287eff5401826c03291677cf7
Mike Ward <mike@mike-ward.net>
2014-07-05 14:41:03 -0400

    Add Sample Site tests

---
+++ Nancy.Pile.Tests/SampleTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nancy.Testing;
+
+namespace Nancy.Pile.Tests
+{
+    [TestClass]
+    public class SampleTests
+    {
+        [TestMethod]
+        public void UnminifiedStyleSheetBundleShouldBeCorrectLength()
+        {
+            var bootstrapper = new Sample.Bootstrapper();
+            var browser = new Browser(bootstrapper);
+            var result = browser.Get("/styles.css", with => with.HttpRequest());
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+            result.Body.ToArray().Length.Should().Be(35356);
+        }
+
+        [TestMethod]
+        public void UnminifiedScriptBundleShouldBeCorrectLength()
+        {
+            var bootstrapper = new Sample.Bootstrapper();
+            var browser = new Browser(bootstrapper);
+            var result = browser.Get("/scripts.js", with => with.HttpRequest());
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+            result.Body.ToArray().Length.Should().Be(133498);            
+        }
+    }
+}
