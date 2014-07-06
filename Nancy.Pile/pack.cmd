d1f4fff4436c7532fbf76f1d3915df0788996b94
Mike Ward <mike@mike-ward.net>
2014-07-06 12:26:32 -0400

    Update pack command to build release target

--- Nancy.Pile/pack.cmd
+++ Nancy.Pile/pack.cmd
@@ -1,2 +1,2 @@
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Nancy.Pile.csproj Configuration=Release
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Nancy.Pile.csproj /property:Configuration=Release
 nuget pack Nancy.Pile.csproj -Prop Configuration=Release
