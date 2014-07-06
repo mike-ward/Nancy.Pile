120c9a0165d2aaaf8b0d5c5d20783f75902f06f4
Mike Ward <mike@mike-ward.net>
2014-07-06 10:21:33 -0400

    Add missing source file

---
+++ Nancy.PIle.Sample.Owin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin.Extensions;
+using Owin;
+
+namespace Nancy.PIle.Sample.Owin
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNancy();
+            app.UseStageMarker(PipelineStage.MapHandler);
+        }
+    }
+}
