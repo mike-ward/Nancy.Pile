84ab03b5829a40f1b2c806513df8bb1db5478ea2
Mike Ward <mike@mike-ward.net>
2014-07-05 20:09:26 -0400

    Add bundle test, test to check if not modified returned

---
+++ Nancy.Pile.Tests/BundleTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Nancy.Pile.Tests
+{
+    [TestClass]
+    public class BundleTests
+    {
+        [TestMethod]
+        public void BuildAssetShouldReturnHashCode()
+        {
+            var hash = Bundle.BuildAssetBundle(new[] {"*.css"}, Bundle.CompressionType.None, ".");
+            hash.Should().Be(186375109);
+        }
+    }
+}
--- Nancy.Pile.Tests/SampleTests.cs
+++ Nancy.Pile.Tests/SampleTests.cs
@@ -32,6 +32,23 @@ namespace Nancy.Pile.Tests
         }
 
         [TestMethod]
+        public void SecondRequestShouldReturnNotModified()
+        {
+            var bootstrapper = new Sample.Bootstrapper();
+            var browser = new Browser(bootstrapper);
+            var result = browser.Get("/scripts.js", with => with.HttpRequest());
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+            var etag = result.Headers["ETag"];
+            var result2 = browser.Get("/scripts.js",
+                with =>
+                {
+                    with.HttpRequest();
+                    with.Header("If-None-Match", etag);
+                });
+            result2.StatusCode.Should().Be(HttpStatusCode.NotModified);
+        }
+
+        [TestMethod]
         public void IndexPageShouldReturnOkStatus()
         {
             var bootstrapper = new Sample.Bootstrapper();
