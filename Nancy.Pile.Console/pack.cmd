86f0f1376ec1a01c7c09d730f6c9fd085e09ab96
mike-ward <mike@mike-ward.net>
2016-03-30 11:07:11 -0400

    add required support files

--- Nancy.Pile.Console/pack.cmd
+++ Nancy.Pile.Console/pack.cmd
@@ -1,2 +1,2 @@
 msbuild.exe Nancy.Pile.Console.csproj /property:Configuration=Release
-IF ERRORLEVEL 0 nuget pack Nancy.Pile.Console.csproj -Prop Configuration=Release
+IF ERRORLEVEL 0 nuget pack Nancy.Pile.Console.csproj -Prop Configuration=Release 
